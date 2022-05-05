9368c41aeb4c5f1b6f59e3567d6a05d546939526
Zachary Coleman <zacharywcoleman@gmail.com>
2022-05-04 22:14:51 -0500

    updating rust code

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,74 @@ use std::iter::zip;
 
 const WORD_LEN: usize = 5;
 const STRAT_NUM: i32 = 3;
+const EMPTY_CHAR_VEC: Vec<char> = vec![];
+
+struct Information {
+    has_set: HashSet<char>,
+    not_set: HashSet<char>,
+    known_pos: [char; WORD_LEN],
+    not_pos: [Vec<char>; WORD_LEN],
+}
+
+impl Information {
+    pub fn new() -> Self {
+        return Self {
+            has_set: HashSet::new(),
+            not_set: HashSet::new(),
+            known_pos: ['_'; WORD_LEN],
+            not_pos: [EMPTY_CHAR_VEC; WORD_LEN],
+        };
+    }
+
+    pub fn update_info(&mut self, hidden_word: &str, guesses: Vec<&str>) {
+        let hidden_set: HashSet<char> = hidden_word.chars().collect();
+        for guess_word in guesses {
+            for (i, (guess_char, hidden_char)) in
+                zip(guess_word.chars(), hidden_word.chars()).enumerate()
+            {
+                // set tests
+                if hidden_set.contains(&guess_char) {
+                    self.has_set.insert(guess_char);
+                } else {
+                    self.not_set.insert(guess_char);
+                }
+
+                // position tests
+                if guess_char == hidden_char {
+                    self.known_pos[i] = guess_char;
+                } else {
+                    self.not_pos[i].push(guess_char);
+                }
+            }
+        }
+    }
+
+    pub fn print(&self) {
+        println!("has set: ");
+        {
+            let tmp: String = self.has_set.clone().into_iter().collect();
+            println!("  {}", tmp);
+        }
+
+        println!("not set:");
+        {
+            let tmp: String = self.not_set.clone().into_iter().collect();
+            println!("  {}", tmp);
+        }
+
+        println!("known pos:");
+        {
+            let tmp: String = self.known_pos.clone().into_iter().collect();
+            println!("  {}", tmp);
+        }
+
+        println!("not pos:");
+        for v in self.not_pos.clone() {
+            let tmp: String = v.into_iter().collect();
+            println!("  {}", tmp);
+        }
+    }
+}
 
 fn main() {
     let lines = match fs::read_to_string("./data/candidates.txt") {
@@ -14,55 +82,7 @@ fn main() {
     let hidden_word = "hairy";
     let guess_words = vec!["adieu", "chair", "wilks"];
 
-    const EMPTY_CHAR_VEC: Vec<char> = vec![];
-    let hidden_set: HashSet<char> = hidden_word.chars().collect();
-
-    let mut has_set: HashSet<char> = HashSet::new();
-    let mut not_set: HashSet<char> = HashSet::new();
-    let mut known_pos = ['_'; WORD_LEN];
-    let mut not_pos: [Vec<char>; WORD_LEN] = [EMPTY_CHAR_VEC; WORD_LEN];
-
-    for guess_word in guess_words {
-        for (i, (guess_char, hidden_char)) in
-            zip(guess_word.chars(), hidden_word.chars()).enumerate()
-        {
-            // set tests
-            if hidden_set.contains(&guess_char) {
-                has_set.insert(guess_char);
-            } else {
-                not_set.insert(guess_char);
-            }
-
-            // position tests
-            if guess_char == hidden_char {
-                known_pos[i] = guess_char;
-            } else {
-                not_pos[i].push(guess_char);
-            }
-        }
-    }
-
-    println!("has set: ");
-    {
-        let tmp: String = has_set.into_iter().collect();
-        println!("  {}", tmp);
-    }
-
-    println!("not set:");
-    {
-        let tmp: String = not_set.into_iter().collect();
-        println!("  {}", tmp);
-    }
-
-    println!("known pos:");
-    {
-        let tmp: String = known_pos.into_iter().collect();
-        println!("  {}", tmp);
-    }
-
-    println!("not pos:");
-    for v in not_pos {
-        let tmp: String = v.into_iter().collect();
-        println!("  {}", tmp);
-    }
+    let mut info = Information::new();
+    info.update_info(hidden_word, guess_words);
+    info.print();
 }
